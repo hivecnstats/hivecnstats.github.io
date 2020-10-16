7fbec2b4b57dd69d15786d2f25a0212f8ed4bfcd
Your Name <you@example.com>
2020-10-16 04:20:05 +0200

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6e1b75c808b59ab8ef9c105f82fe0f7e.js"
+  "/precache-manifest.78dec7b9a46690001d31247e45572d81.js"
 );
 
 self.addEventListener('message', (event) => {
